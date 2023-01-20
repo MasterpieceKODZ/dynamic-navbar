3508404c97632f6abc74f8642fa5e2baef480d5b
MasterpieceKODZ <grail.masterpiece@gmail.com>
2023-01-20 23:06:20 +0100

    I added box-shadow css prop to .active selector and added the navbar background change on scroll function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const navToggleBtn = document.querySelector(".nav-toggle");
 const navbarRoot = document.querySelector(".navbar-root");
 
+// open the navbar nav on small screen when the toggle button is clicked
 navToggleBtn.addEventListener("click", (e) => {
 	navbarRoot.classList.toggle("open");
 });
+
+// get the current height of the navbar root
+const navHeight = navbarRoot.getBoundingClientRect().height;
+
+//when page is scrolled upwards to half of the height of the navbar the navbar background should turn to white
+window.onscroll = (e) => {
+	if (e.currentTarget.scrollY > navHeight / 2)
+		navbarRoot.classList.add("active");
+	else navbarRoot.classList.remove("active");
+};
